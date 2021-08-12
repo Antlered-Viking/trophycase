e8fb8ad002b8368fc2c65cf713bb1e03b8126769
NeonSpork <leiknes@gmail.com>
2021-08-13 00:33:22 +0200

    refactor: angular flexbox and positioning
    Moved billboard into header.
    Overhaul of components to move flexbox
    from CSS to inline angular
    Positioning tweaks

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -10,11 +10,38 @@ export class GalleryComponent implements OnInit {
   showPoly: boolean;
 
   ethShowcase: any[];
+  polyShowcase: any[];
 
   constructor() {
     this.showEth = true;
     this.showPoly = true;
     this.ethShowcase = [
+      {
+        thumbURL:
+          'https://lh3.googleusercontent.com/7MhlCBpslKYY22r3XSUIpD0vCKMzbfQB4qfDVXZ6Ibz4okQ4UHiw840oU4XW_ETy0xuCVo8yP7XGZCzwZrrKxK50VG5yQDmEnrQn',
+        title: 'Eighteen',
+        author: 'lonelysince67',
+      },
+      {
+        thumbURL:
+          'https://lh3.googleusercontent.com/7MhlCBpslKYY22r3XSUIpD0vCKMzbfQB4qfDVXZ6Ibz4okQ4UHiw840oU4XW_ETy0xuCVo8yP7XGZCzwZrrKxK50VG5yQDmEnrQn',
+        title: 'Eighteen',
+        author: 'lonelysince67',
+      },
+      {
+        thumbURL:
+          'https://lh3.googleusercontent.com/7MhlCBpslKYY22r3XSUIpD0vCKMzbfQB4qfDVXZ6Ibz4okQ4UHiw840oU4XW_ETy0xuCVo8yP7XGZCzwZrrKxK50VG5yQDmEnrQn',
+        title: 'Eighteen',
+        author: 'lonelysince67',
+      },
+      {
+        thumbURL:
+          'https://lh3.googleusercontent.com/7MhlCBpslKYY22r3XSUIpD0vCKMzbfQB4qfDVXZ6Ibz4okQ4UHiw840oU4XW_ETy0xuCVo8yP7XGZCzwZrrKxK50VG5yQDmEnrQn',
+        title: 'Eighteen',
+        author: 'lonelysince67',
+      }
+    ];
+    this.polyShowcase = [
       {
         thumbURL:
           'https://lh3.googleusercontent.com/7MhlCBpslKYY22r3XSUIpD0vCKMzbfQB4qfDVXZ6Ibz4okQ4UHiw840oU4XW_ETy0xuCVo8yP7XGZCzwZrrKxK50VG5yQDmEnrQn',
